fdf5cd114da4681aa116123e2bc4701805c9b0ce
Phoebe✰ <vonenzell@gmail.com>
2026-04-20 13:10:16 +0300

    Set up virtual environment and install dependencies

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def home():
+    return {"message": "API is working!"}

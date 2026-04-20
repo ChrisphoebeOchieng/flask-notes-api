a3d8f049fa9463ee00bcab383ae89867caee1b27
Phoebe✰ <vonenzell@gmail.com>
2026-04-20 13:19:18 +0300

    Update database path and clean import formatting

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from models import db,bcrypt
+from models import db, bcrypt
 
 db.init_app(app)
 bcrypt.init_app(app)

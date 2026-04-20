9903a17716559599177d9b608faee49fcbcada01
Phoebe✰ <vonenzell@gmail.com>
2026-04-20 13:39:00 +0300

    Set up JWT authentication configuration

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+
 
 app = Flask(__name__, instance_relative_config=True)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'super-secret-key'  
 
 from models import db, bcrypt
 from models import User, Note
